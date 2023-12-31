e8bd5d822629614e9b58129d08546bbdf7c498c7
Diaznaufal <diaznaufal1@gmail.com>
2023-12-31 18:35:54 +0700

    first push

--- Rumussegitiga.js
+++ Rumussegitiga.js
@@ -1,4 +1,4 @@
-// script.js
+// javascript.js
 function calculate(event) {
     event.preventDefault();
 
@@ -9,16 +9,20 @@ function calculate(event) {
 
     if (!isNaN(base) && !isNaN(height)) {
         var result = 0;
+        var resultContainer = document.getElementById('result');
 
         if (category === 'area') {
             result = 0.5 * base * height; // Rumus luas segitiga
+            resultContainer.innerText = 'Hasil Luas Segitiga: ' + result;
         } else if (category === 'perimeter') {
-            // Tambahkan keliling jika kategori adalah keliling segitiga
-            result = base + height + perimeter; // Rumus keliling segitiga
+            if (!isNaN(perimeter)) {
+                result = base + height + perimeter; // Rumus keliling segitiga
+                resultContainer.innerText = 'Hasil Keliling Segitiga: ' + result;
+            } else {
+                resultContainer.innerText = 'Masukkan nilai yang valid untuk keliling';
+            }
         }
 
-        var resultContainer = document.getElementById('result');
-        resultContainer.innerText = 'Hasil: ' + result;
         resultContainer.style.border = '1px solid #ccc';
         resultContainer.style.borderRadius = '8px';
         resultContainer.style.padding = '10px';
@@ -27,14 +31,11 @@ function calculate(event) {
     }
 }
 
-function selectShape(shape) {
-    console.log('Selected Shape:', shape);
+function resetForm() {
+    document.getElementById('categoryForm').reset();
+    document.getElementById('result').innerText = ''; // Reset kolom hasil
 }
 
-function toggleInputs() {
-    var category = document.getElementById('category').value;
-    var perimeterInput = document.getElementById('perimeter');
-
-    // Sembunyikan/munculkan input keliling berdasarkan kategori
-    perimeterInput.style.display = category === 'area' ? 'none' : 'block';
+function selectShape(shape) {
+    console.log('Selected Shape:', shape);
 }
